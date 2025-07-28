d038fb1b4d8cb6f55a6d2abd02ad92f67113bb43
Souvik Das <dassouvik180@gmail.com>
2025-07-28 13:54:30 +0530

    changes done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,27 +1,54 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
-        <meta name="description" content="NoMoo is your all-in-one app to discover, order, and book plant-powered meals while making a real impact on the planet. Join the plant-based revolution!" />
-        <meta name="keywords" content="plant-based, vegan, sustainable, food delivery, eco-friendly, NoMoo, restaurant booking, healthy eating" />
-        <meta property="og:title" content="NoMoo | Plant-Powered Meals & Sustainable Living" />
-        <meta property="og:description" content="Discover, order, and book plant-powered meals with NoMoo. Make a real impact on the planet while enjoying delicious food." />
-        <meta property="og:image" content="/footerAssets/logowithname.svg" />
-        <meta property="og:url" content="https://nomoo.app/" />
+        <meta
+          name="description"
+          content="NoMoo is your all-in-one app to discover, order, and book plant-powered meals while making a real impact on the planet. Join the plant-based revolution!"
+        />
+        <meta
+          name="keywords"
+          content="plant-based, vegan, sustainable, food delivery, eco-friendly, NoMoo, restaurant booking, healthy eating"
+        />
+        <meta
+          property="og:title"
+          content="NoMoo | Plant-Powered Meals & Sustainable Living"
+        />
+        <meta
+          property="og:description"
+          content="Discover, order, and book plant-powered meals with NoMoo. Make a real impact on the planet while enjoying delicious food."
+        />
+        <meta
+          property="og:image"
+          content="https://mynomoo.com/light-thumbnail.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.mynomoo.com/" />
+        <meta
+          property="twitter:title"
+          content="PNoMoo | Plant-Powered Meals & Sustainable Living"
+        />
+        <meta
+          property="twitter:description"
+          content="Discover, order, and book plant-powered meals with NoMoo. Make a real impact on the planet while enjoying delicious food."
+        />
+        <meta
+          property="twitter:image"
+          content="https://mynomoo.com/light-thumbnail.png"
+        />
+        <meta property="og:url" content="https://mynomoo.com/" />
         <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="NoMoo | Plant-Powered Meals & Sustainable Living" />
-        <meta name="twitter:description" content="Discover, order, and book plant-powered meals with NoMoo. Make a real impact on the planet while enjoying delicious food." />
-        <meta name="twitter:image" content="/footerAssets/logowithname.svg" />
+
         <link rel="canonical" href="https://nomoo.app/" />
         <meta name="theme-color" content="#00C853" />
       </Head>
-      <body className='antialiased'>
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
